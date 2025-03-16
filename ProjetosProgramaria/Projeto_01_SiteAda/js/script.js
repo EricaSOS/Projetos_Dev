374445f48f2daf3feeffdacf350beee32f1e7494
EricaSOS <erica.sos.adv@hotmail.com>
2025-03-16 14:02:55 -0300

    fix: Correção no label e input do index.html, para coincidir com o que está no alert do arquivo script.js

--- ProjetosProgramaria/Projeto_01_SiteAda/js/script.js
+++ ProjetosProgramaria/Projeto_01_SiteAda/js/script.js
@@ -1,9 +1,12 @@
 document.getElementById("botaoEnviar").addEventListener("click", validarFormulario)
 
 function validarFormulario(){
-  if(document.getElementById("nome-da-usuaria").value != "" && document.getElementById("email-da-usuaria").value != "" && document.getElementById("telefone-da-usuaria").value != ""){
+  if(document.getElementById("nome-da-usuaria").value != "" 
+        && document.getElementById("email-da-usuaria").value != "" 
+        && document.getElementById("telefone-da-usuaria").value != "")
+  {
     alert("Prontinho! Você receberá as novidades por e-mail.")
   }else{
-    alert("Por favor, preencha os campos nome e email!")
+    alert("Por favor, preencha os campos nome, email e/ou telefone!")
   }
 }
